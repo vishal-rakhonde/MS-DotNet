57d17b16a14ec4a2e7746885139973221eaefbbc
vishal-rakhonde <vrakhondepatil@gmail.com>
2024-08-08 20:30:57 +0530

    cookie

---
+++ CookiesExample/Models/DummyData.cs
@@ -0,0 +1,8 @@
+﻿namespace CookiesExample.Models
+{
+    public class DummyData
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ CookiesExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CookiesExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
