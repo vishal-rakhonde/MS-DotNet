b138541af8db5063ac93b5e667228d03e44ac4a5
vishal-rakhonde <vrakhondepatil@gmail.com>
2025-01-20 22:13:51 +0530

    CRUD

---
+++ CRUDOperation/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUDOperation.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int age {  get; set; }
+    }
+}
